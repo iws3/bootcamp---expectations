bed2e2ea13f99fa28517570f7c7c8871f3e1ec91
Deoum <favourtayuh@gmail.com>
2025-07-22 21:34:04 +0100

    I have added a file called Deoum.py and included a line of code in it

--- expectations/Deoum.py
+++ expectations/Deoum.py
@@ -1,1 +1,1 @@
-print("At the end of the Ultimate GenAI Course Bamenda 2025, I expect to <write your expectation here!>")
+print("At the end of the Ultimate GenAI Course Bamenda 2025, I expect to be able to use the knowledge i recieve in building and solving real world problems. Starting from the ones faced in our community")
